f91b095daf91756193d3a34e2dfc8ace4e05abb1
刘雷 <liulei13@zuoyebang.com>
2021-01-15 17:21:51 +0800

    添加chainWebpack

---
+++ src/pages/console/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <index-view />
+  </div>
+</template>
+
+<script>
+import IndexView from './view/index'
+export default {
+  name: 'App',
+  components: {
+    IndexView,
+  },
+}
+</script>
+
+<style>
+body {
+  background-color: #f8f8fe;
+}
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,24 @@
+
+const v1Config = require('./config/index.js')
+
+
+const pages= {
+    index: {
+        entry: 'src/pages/client/index.js',
+        template: 'public/index.html',
+      },
+      console: {
+        entry: 'src/pages/console/index.js',
+        template: 'public/index.html',
+      },
+}
+module.exports = {
+    pages,
+    publicPath:process.env.NODE_ENV === 'production'
+    ? v1Config.staticPath
+    : '/static/hy/yaya-main/',
+    devServe:{},
+    chainWebpack:{
+        // webpack 抛出的配置
+    }
+}
